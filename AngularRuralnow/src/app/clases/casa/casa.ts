e81799637e95df82ee5b06af5a914881f630c2d6
Sara.Pedrosa <sarpdrui@outlook.com>
2020-11-03 16:46:16 +0100

    servicio casa creado

---
+++ AngularRuralnow/src/app/clases/casa/casa.ts
@@ -0,0 +1,13 @@
+export class Casa {
+    id: number;
+    nombre: string;
+    municipio: number;
+    descripcion: string;
+    personas: number
+    habitaciones: number;
+    camas: number;
+    aseos: number;
+    precio_noche: number;
+    propietario: number;
+}
+
