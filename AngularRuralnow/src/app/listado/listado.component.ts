efff54bf49dd6583520f10110ca2223031345c66
jmarcosgh <72085838+Markofen18@users.noreply.github.com>
2020-11-03 17:02:22 +0100

    Component listado

---
+++ AngularRuralnow/src/app/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oevents-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.scss']
+})
+export class ListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
