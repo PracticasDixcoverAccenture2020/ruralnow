39786972dc5ff397044173eb3ba9da20347d80c6
Sara.Pedrosa <sarpdrui@outlook.com>
2020-11-03 10:43:42 +0100

    personalizando toolbar

--- AngularRuralnow/src/app/app.module.ts
+++ AngularRuralnow/src/app/app.module.ts
@@ -4,9 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 import 'hammerjs';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
@@ -21,7 +20,9 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatListModule 
+    MatListModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
