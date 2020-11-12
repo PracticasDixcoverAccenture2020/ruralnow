17e75932805ae0482018ac44d0b9e577df529298
SaraPdrui <sarapdrui@outlook.com>
2020-11-12 16:12:54 +0100

    Servicios-Component listo

--- AngularRuralnow/src/app/app-routing.module.ts
+++ AngularRuralnow/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from "@angular/router";
 
 //components
 import { LandingPageComponent } from "src/app/components/landing-page/landing-page.component";
-import { PageNotFoundComponentComponent } from "src/app/page-not-found-component/page-not-found-component.component";
+import { PageNotFoundComponentComponent } from "src/app/components/page-not-found-component/page-not-found-component.component";
 import {ListadoCasasComponent} from "src/app/components/listado-casas/listado-casas.component";
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { NuestrosServiciosComponent } from './components/nuestros-servicios/nuestros-servicios.component';
+
 
 
 
@@ -14,6 +16,7 @@ const routes: Routes = [
   { path: "home", component: LandingPageComponent },
   { path: "houses", component: ListadoCasasComponent },
   { path: "contact", component: ContactUsComponent },
+  { path: "servicios", component: NuestrosServiciosComponent },
 
   
   { path: "", redirectTo: "/home", pathMatch: "full" },
---
+++ AngularRuralnow/src/app/components/nuestros-servicios/nuestros-servicios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oevents-nuestros-servicios',
+  templateUrl: './nuestros-servicios.component.html',
+  styleUrls: ['./nuestros-servicios.component.scss']
+})
+export class NuestrosServiciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
